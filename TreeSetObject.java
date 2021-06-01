aea5a2e7f75e736372c5e368b299ff92c303a779
Latha Murugan <81089023+LathaMurugan-code@users.noreply.github.com>
2021-06-01 21:35:45 +0530

    object class for treeset

---
+++ TreeSetObject.java
@@ -0,0 +1,18 @@
+package collectionpackage;
+
+public class TreeSetObject {
+	String Trainee;
+	String Course;
+	int Fees;
+	public TreeSetObject(String Trainee,String Course,int Fees)
+	{
+		this.Trainee = Trainee;
+		this.Course = Course;
+		this.Fees   = Fees;
+		
+	}
+
+	public String toString() {
+		return this.Trainee+" "+this.Course+" "+this.Fees;
+	}
+}

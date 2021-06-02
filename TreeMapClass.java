26c9a6e31ed4509f95ae54b275fd5018254ba109
Latha Murugan <81089023+LathaMurugan-code@users.noreply.github.com>
2021-06-02 21:26:08 +0530

    TreeMap

---
+++ TreeMapClass.java
@@ -0,0 +1,21 @@
+package collectionpackage;
+
+import java.util.TreeMap;
+
+public class TreeMapClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeMap t = new TreeMap();
+		t.put("latha",98);
+		t.put("vidhu",96);
+		t.put("Raja", 94);
+		t.put("vimal", 89);
+		//Ascending order of keys maintained
+		//heterogenous keys not allowed
+		System.out.println(t);
+		
+
+	}
+
+}

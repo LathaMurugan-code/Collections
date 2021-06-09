3ed953548b46edf193a262e9fcd73895561f52d5
Latha Murugan <81089023+LathaMurugan-code@users.noreply.github.com>
2021-06-09 21:14:21 +0530

    Frequency check with hashmap

---
+++ InterviewProgram.java
@@ -0,0 +1,40 @@
+package collectionpackage;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class InterviewProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String Name = "lathathal";
+		char []c = Name.toCharArray();
+		HashMap <Character,Integer>h = new HashMap<Character,Integer>();
+		for (int i=0;i<c.length;i++)
+		{
+			if(h.containsKey(c[i])==false)
+			h.put(c[i], 1);
+			else
+			{
+				int value = h.get(c[i]);
+				h.put(c[i], value+1);
+			}
+		}
+		Set<Map.Entry<Character, Integer>> s = h.entrySet();
+		
+		for(Map.Entry<Character,Integer> e:s)
+		{
+			if(e.getValue()==3)
+				
+				{
+				e.setValue(99);
+				}
+				System.out.println(e);
+			
+		}
+
+	}
+
+}
